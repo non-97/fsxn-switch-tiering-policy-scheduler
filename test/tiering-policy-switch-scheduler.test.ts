089a0640a394001c8af424e0de6a60336eef2b4a
non-97 <nonmonaka07@gmail.com>
2024-04-07 15:23:11 +0900

    Initial commit

---
+++ test/tiering-policy-switch-scheduler.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TieringPolicySwitchScheduler from '../lib/tiering-policy-switch-scheduler-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tiering-policy-switch-scheduler-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TieringPolicySwitchScheduler.TieringPolicySwitchSchedulerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
